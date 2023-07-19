71bc371a5b0973b4cc63cbaf68f86d1fef03f0f0
HaithemBenSlama <haithem.benslama@hotmail.fr>
2023-07-19 14:17:13 +0100

    Product Not found section

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -6,9 +6,16 @@ import { Breadcrumbs } from "@/components/Breadcrumbs";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import RightSide from "./component/RightSide";
+import { usePathname } from "next/navigation";
+import { GrFormPreviousLink } from "react-icons/gr";
+import { PiSmileySadLight } from "react-icons/pi";
 
 const page = () => {
-  const item = dataProduct;
+  const pathname = usePathname();
+  const parts = pathname.split("/"); // Split the URL by "/"
+  const productId = parseInt(parts[parts.length - 1]);
+  const data = dataProduct;
+  const item = productId ? data.find((d) => d.p_id === productId) : null;
   const [refreshCart, setRefreshCart] = useState(false);
 
   return (
@@ -16,22 +23,47 @@ const page = () => {
       <Navbar refreshCart={refreshCart} setRefreshCart={setRefreshCart} />
       <div className="pt-10">
         <section className="pt-12 pb-24 bg-blueGray-100 rounded-b-10xl overflow-hidden">
-          <div className="container px-4 mx-auto">
-            <div className="flex flex-wrap -mx-4">
-              <div className="w-full px-4 py-5">
-                <Breadcrumbs
-                  path={[{ text: "Store", href: "/Store" }]}
-                  terminalPath={item?.p_name}
+          {item ? (
+            <div className="container px-4 mx-auto">
+              <div className="flex flex-wrap -mx-4">
+                <div className="w-full px-4 py-5">
+                  <Breadcrumbs
+                    path={[{ text: "Store", href: "/Store" }]}
+                    terminalPath={item?.p_name}
+                  />
+                </div>
+                <LeftContent data={item} />
+                <RightSide
+                  item={item}
+                  refreshCart={refreshCart}
+                  setRefreshCart={setRefreshCart}
                 />
               </div>
-              <LeftContent data={item} />
-              <RightSide
-                item={item}
-                refreshCart={refreshCart}
-                setRefreshCart={setRefreshCart}
-              />
             </div>
-          </div>
+          ) : (
+            <div className="py-5 px-5">
+              <div className="flex items-center justify-center p-5 md:p-0">
+                <div className="flex flex-col items-center justify-center bg-slate-50 w-[600px] rounded-2xl shadow-xl h-72">
+                  <div className="text-3xl text-gray-500">
+                    <PiSmileySadLight className="w-28 h-28" />
+                  </div>
+                  <div className="text-2xl md:text-4xl text-gray-500 mt-10">
+                    Oups ! Product Not Found
+                  </div>
+                </div>
+              </div>
+              <div className="md:w-72 hover:cursor-pointer">
+                <a
+                  type="button"
+                  className="flex items-center justify-center gap-5 py-5 px-5 w-full text-xl leading-6 font-medium tracking-tighter bg-white hover:bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                  href="/Home"
+                >
+                  <GrFormPreviousLink />
+                  Back to shop
+                </a>
+              </div>
+            </div>
+          )}
         </section>
       </div>
       <Footer />
--- app/product/[id]/variables/data.js
+++ app/product/[id]/variables/data.js
@@ -1,33 +1,35 @@
-export const dataProduct = {
-  p_id: 15,
-  p_sku: "#MGN63FN/A",
-  p_photo:
-    "https://i5.walmartimages.com/asr/f702f146-783f-4710-80d0-c46c57cf8e36.ef46df5ab249f27b3b66445bbd189642.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
-  p_name: "IPHONE 14 PRO MAX 128GO",
-  p_img2:
-    "https://i5.walmartimages.com/asr/62ab58a0-265e-4c3d-960d-15d07b70074e.a005f33016de28c7db514b94220f032a.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
-  p_img3:
-    "https://i5.walmartimages.com/asr/6be0258e-c015-437b-82b4-0a8e600fa563.6b2b814b7a8356efcd9f7b5e8df04620.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
-  p_img4:
-    "https://i5.walmartimages.com/asr/7c40ae51-149d-4f8e-8201-88e7aa46cb21.005c7ccef83451e5f33294fbe0e237af.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
-  p_desc:
-    "• 6.7-inch Super Retina XDR display2 with ProMotion and always-on screen• Dynamic Island, a new and magical way to interact with your iPhone• 48MP main camera for up to 4x higher resolution• Cinematic mode, now in 4K Dolby Vision up to 30fps• Action mode, for stable and smooth videos when you're on the move• Accident detection1, a vital safety technology that calls for help for you",
-  p_price: 3999,
-  p_category: "Phone",
-  p_stock: 1,
-  p_brand: "Apple",
-  p_rating: 3.5,
-  p_sales: "1.6M",
-  p_revenue: "$3.5M",
-  p_discount: 0.13,
-  p_nb_reviews: 100,
-  p_feedback: [
-    {
-      p_feed_id: 1,
-      p_user_name: "Haithem ben Slama",
-      p_user_rate: 3.5,
-      p_review_date: "Apr 16,2023",
-      p_review_msg: "Good Service !",
-    },
-  ],
-};
+export const dataProduct = [
+  {
+    p_id: 15,
+    p_sku: "#MGN63FN/A",
+    p_photo:
+      "https://i5.walmartimages.com/asr/f702f146-783f-4710-80d0-c46c57cf8e36.ef46df5ab249f27b3b66445bbd189642.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+    p_name: "IPHONE 14 PRO MAX 128GO",
+    p_img2:
+      "https://i5.walmartimages.com/asr/62ab58a0-265e-4c3d-960d-15d07b70074e.a005f33016de28c7db514b94220f032a.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+    p_img3:
+      "https://i5.walmartimages.com/asr/6be0258e-c015-437b-82b4-0a8e600fa563.6b2b814b7a8356efcd9f7b5e8df04620.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+    p_img4:
+      "https://i5.walmartimages.com/asr/7c40ae51-149d-4f8e-8201-88e7aa46cb21.005c7ccef83451e5f33294fbe0e237af.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+    p_desc:
+      "• 6.7-inch Super Retina XDR display2 with ProMotion and always-on screen• Dynamic Island, a new and magical way to interact with your iPhone• 48MP main camera for up to 4x higher resolution• Cinematic mode, now in 4K Dolby Vision up to 30fps• Action mode, for stable and smooth videos when you're on the move• Accident detection1, a vital safety technology that calls for help for you",
+    p_price: 3999,
+    p_category: "Phone",
+    p_stock: 1,
+    p_brand: "Apple",
+    p_rating: 3.5,
+    p_sales: "1.6M",
+    p_revenue: "$3.5M",
+    p_discount: 0.13,
+    p_nb_reviews: 100,
+    p_feedback: [
+      {
+        p_feed_id: 1,
+        p_user_name: "Haithem ben Slama",
+        p_user_rate: 3.5,
+        p_review_date: "Apr 16,2023",
+        p_review_msg: "Good Service !",
+      },
+    ],
+  },
+];
